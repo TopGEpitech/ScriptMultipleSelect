26c703349a9581965aa87ff1d2f2a9c2c7fb7e07
TopGEpitech <118722848+TopGEpitech@users.noreply.github.com>
2023-02-08 20:55:35 +0100

    Add files via upload

---
+++ multiple.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#hobbies option').mousedown(function(e) {
+        e.preventDefault();
+        var originalScrollTop = $(this).parent().scrollTop();
+        console.log(originalScrollTop);
+        $(this).prop('selected', $(this).prop('selected') ? false : true);
+        var self = this;
+        $(this).parent().focus();
+        setTimeout(function() {
+            $(self).parent().scrollTop(originalScrollTop);
+        }, 0);
+        return false;
+    });
+});
